62e3e0160681c5b651d5bd6d09f670987c360d9b
rohan <rohandensilf@gmail.com>
2023-04-10 11:49:06 +0530

    created juicyfresh project

---
+++ juicy_fresh/juicy_fresh/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for juicy_fresh project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'juicy_fresh.settings')
+
+application = get_asgi_application()
