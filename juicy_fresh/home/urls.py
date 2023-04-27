4d5f2b6195fbb9855e413e087f50a525626a451f
rohan <rohandensilf@gmail.com>
2023-04-27 12:46:20 +0530

    made login and register pages bug free, displays error messages

--- juicy_fresh/home/urls.py
+++ juicy_fresh/home/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.index),
     path('xyz/',views.test),
-    path('login/',views.loginpage),
-    path('login/loginsub/',views.loginsub),
-    path('register/',views.registerpage),
-    path('register/registersub/',views.registersub)
+    path('login/',views.login,name="loginpage"),
+    path('register/',views.register,name="registerpage"),
 ]
--- juicy_fresh/home/views.py
+++ juicy_fresh/home/views.py
@@ -10,39 +10,41 @@ def index(request):
 def test(r):
     return render(r,'test.html',{'val':'java'})
 
-def loginpage(r):
-    return render(r,'login.html')
-
-def registerpage(r):
-    return render(r,'register.html')
-
-def loginsub(request):
-    uname = request.POST['uname']
-    pword = request.POST['pword']
-    user = auth.authenticate(username=uname,password=pword)
-    if user:
-        auth.login(request,user)
-        return redirect('/')
-    return render(request,'login.html')
-
-def registersub(request):
-    uname = request.POST['uname']
-    fname = request.POST['fname']
-    lname = request.POST['lname']
-    email = request.POST['email']
-    pword = request.POST['pword']
-    rpword = request.POST['rpword']
-    if pword == rpword:
-        if User.objects.filter(username=uname):
-            msg = "Username is already taken"
-            return render(request,'test.html',{'val':msg})
-        elif User.objects.filter(email=email):
-            msg = "E-mail is already registered"
-            return render(request,'test.html',{'val':msg})
-        else:
-            user=User.objects.create_user(first_name=fname,last_name=lname,password=pword,username=uname,email=email)
-            user.save();
+def login(request):
+    if request.method=='POST':
+        uname = request.POST['uname']
+        pword = request.POST['pword']
+        user = auth.authenticate(username=uname,password=pword)
+        if user:
+            auth.login(request,user)
             return redirect('/')
+        msg = "Invalid username and password"
+        return render(request,'login.html',{'msg':msg})
     else:
-        msg = "Passwords do not match"
-        return render(request,'test.html',{'val':msg})
+        return render(request,'login.html')
+
+def register(request):
+    if request.method=='POST':
+        uname = request.POST['uname']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pword = request.POST['pword']
+        rpword = request.POST['rpword']
+        if pword == rpword:
+            if User.objects.filter(username=uname):
+                msg = "Username is already taken"
+                return render(request,'register.html',{'msg':msg})
+            elif User.objects.filter(email=email):
+                msg = "E-mail is already registered"
+                return render(request,'register.html',{'msg':msg})
+            else:
+                user=User.objects.create_user(first_name=fname,last_name=lname,password=pword,username=uname,email=email)
+                user.save();
+                auth.login(request,user)
+                return redirect('/')
+        else:
+            msg = "Passwords do not match"
+            return render(request,'register.html',{'msg':msg})
+    else:
+        return render(request,'register.html')
