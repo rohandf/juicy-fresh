98b5d8e80a1f43db2177a63ab214969d89167902
Rohandensil <129720330+Rohandensil@users.noreply.github.com>
2023-07-01 21:13:40 +0530

    Remove Django Secret Key

--- juicy_fresh/juicy_fresh/settings.py
+++ juicy_fresh/juicy_fresh/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-trwz0xsc$y+@*&##3gx3*-2ov9$260tf_v+xjjzm_)ejiod91z'
+SECRET_KEY = '##########################################################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
